2bb5b18e38577b2b979cab17e3c8a01720cb8bc4
BJ Miller <bj@sixfivesoftware.com>
2020-07-02 10:10:03 -0400

    Initial Commit

---
+++ MiniNetworkLayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniNetworkLayer
+//
+//  Created by BJ Miller on 7/2/20.
+//  Copyright © 2020 BJ Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
